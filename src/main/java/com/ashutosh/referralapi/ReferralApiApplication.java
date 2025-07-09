72b3eccbc5d0fe70a6afe813675185bbe33e5069
ashutosh050403 <ashu.maur678678@gmail.com>
2025-07-09 14:12:43 +0530

    Project Created

---
+++ src/main/java/com/ashutosh/referralapi/ReferralApiApplication.java
@@ -0,0 +1,13 @@
+package com.ashutosh.referralapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReferralApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReferralApiApplication.class, args);
+    }
+
+}
